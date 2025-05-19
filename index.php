55e0a58387332be494a27290e6e6ed0786b93428
Milaim Hajni (Zeka) <amararifi1234@gmail.com>
2025-05-19 10:08:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$sports = ["Football","Basketball","Voleyball","Handball"];
+
+// $len = count($sports);
+
+// for($i=0; $i < $len; $i++){
+//    echo $sports[$i] . "<br>";
+// }
+
+
+// array_push($sports, "Golf");
+// var_dump($sports);
+
+// array_pop($sports);
+// var_dump($sports);
+
+// array_unshift($sports, "Golf");
+// var_dump($sports);
+
+// array_slice($value,3,4);
+// var_dump($value);
+
+// array_shift($sports);
+// var_dump($sports);
+
+// $value = [50,50,10,20,50];
+
+// echo array_sum($value);
+
